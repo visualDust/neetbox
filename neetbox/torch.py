38c51693a45b9e17479afb73c8fca78b72cf6981
VisualDust <gavin@gong.host>
2023-03-07 22:38:51 +0800

    fixed benchmark issues

--- neetbox/torch.py
+++ neetbox/torch.py
@@ -6,7 +6,7 @@ import torch
 
 logger = get_logger(whom="NEETBOX")
 
-def profile(model, input_resolution=(2048,1024), profiling = True, calc_fps = True, speedtest = 1):
+def profile(model, input_resolution=(2048,1024), profiling = True, calc_fps = 1000, speedtest = 1):
     if profiling:
         logger.log("Model profiling...")
         tensor = torch.rand(1, 3, input_resolution[1], input_resolution[0])
@@ -21,27 +21,28 @@ def profile(model, input_resolution=(2048,1024), profiling = True, calc_fps = Tr
             tensor = torch.rand(1, 3, input_resolution[1], input_resolution[0])
             if next(model.parameters()).is_cuda:
                 tensor = tensor.cuda()
+            logger.log("Calculating FPS...")
             t0 = time.time()
-            for _ in tqdm(range(1000)):
+            for _ in tqdm(range(calc_fps)):
                 model(tensor)
             t1 = time.time()
-            logger.log(f"FPS:{1000 / (t1 - t0)}, Time:{(t1 - t0):%2f}")
-    if not speedtest:
-        return
-    logger.log("Attention that model speedtest results are different on different devices or under different circumstances.")
-    model.eval()
-    tensor = torch.rand(1, 3, input_resolution[1], input_resolution[0])
-    if next(model.parameters()).is_cuda:
-        tensor = tensor.cuda()
-    _ = model(tensor)
-    counter = []
-    for test_index in range(speedtest+2):
-        t = time.time()
-        model(tensor)
-        counter.append(time.time() - t)
-        logger.log(f'SpeedTest: {counter[-1]:.4f}s in {test_index+1}st run.')
-    if len(counter):
-        _min, _max = min(counter), max(counter)
-        aver = (sum(counter) - _min - _max) / (len(counter) - 2)
-        logger.log(f"SpeedTest: average {aver}s per run with input size {input_resolution}")
-    
+            logger.log(f"FPS: {calc_fps / (t1 - t0)}")
+    if speedtest:
+        logger.log("Running SpeedTest...")
+        logger.log("Attention that model speedtest results are different on different devices or under different circumstances.")
+        model.eval()
+        tensor = torch.rand(1, 3, input_resolution[1], input_resolution[0])
+        if next(model.parameters()).is_cuda:
+            tensor = tensor.cuda()
+        _ = model(tensor)
+        counter = []
+        for test_index in range(speedtest+2):
+            t = time.time()
+            model(tensor)
+            counter.append(time.time() - t)
+            logger.log(f'SpeedTest: {counter[-1]:.4f}s in {test_index+1}st run.')
+        if len(counter):
+            _min, _max = min(counter), max(counter)
+            aver = (sum(counter) - _min - _max) / (len(counter) - 2)
+            logger.log(f"SpeedTest: average {aver}s per run with input size {input_resolution}")
+            logger.log(f"Min inference time: {_min}s, Max inference time:{_max}")
