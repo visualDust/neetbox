e81eac687638694cb499c8515d8c7e3fab7b928d
VisualDust <gavin@gong.host>
2023-03-07 22:18:24 +0800

    added model profiling

---
+++ neetbox/torch.py
@@ -0,0 +1,47 @@
+from thop import profile as _profile
+from neetbox.logger import get_logger
+import time
+from tqdm import tqdm
+import torch
+
+logger = get_logger(whom="NEETBOX")
+
+def profile(model, input_resolution=(2048,1024), profiling = True, calc_fps = True, speedtest = 1):
+    if profiling:
+        logger.log("Model profiling...")
+        tensor = torch.rand(1, 3, input_resolution[1], input_resolution[0])
+        if next(model.parameters()).is_cuda:
+            tensor = tensor.cuda()
+        tensor = (tensor,)
+        flops, params = _profile(model, inputs=tensor)
+        logger.log(f"Model FLOPs = {flops/1e9}, params = {params/1e6}")
+    if calc_fps:
+        model.eval()
+        with torch.no_grad():
+            tensor = torch.rand(1, 3, input_resolution[1], input_resolution[0])
+            if next(model.parameters()).is_cuda:
+                tensor = tensor.cuda()
+            t0 = time.time()
+            for _ in tqdm(range(1000)):
+                model(tensor)
+            t1 = time.time()
+            logger.log(f"FPS:{1000 / (t1 - t0)}, Time:{(t1 - t0):%2f}")
+    if not speedtest:
+        return
+    logger.log("Attention that model speedtest results are different on different devices or under different circumstances.")
+    model.eval()
+    tensor = torch.rand(1, 3, input_resolution[1], input_resolution[0])
+    if next(model.parameters()).is_cuda:
+        tensor = tensor.cuda()
+    _ = model(tensor)
+    counter = []
+    for test_index in range(speedtest+2):
+        t = time.time()
+        model(tensor)
+        counter.append(time.time() - t)
+        logger.log(f'SpeedTest: {counter[-1]:.4f}s in {test_index+1}st run.')
+    if len(counter):
+        _min, _max = min(counter), max(counter)
+        aver = (sum(counter) - _min - _max) / (len(counter) - 2)
+        logger.log(f"SpeedTest: average {aver}s per run with input size {input_resolution}")
+    
