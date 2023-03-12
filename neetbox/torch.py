4cd048ac330c78b91943ce215be689abf1f3aa49
VisualDust <gavin@gong.host>
2023-03-12 23:18:02 +0800

    added GPU side synchronized speed test

--- neetbox/torch.py
+++ neetbox/torch.py
@@ -6,31 +6,62 @@ import torch
 
 logger = get_static_logger()
 
-def profile(model, input_shape=(1,3,2048,1024), profiling = True, speedtest = 1000):
-    tensor = torch.rand(input_shape) # creating input tensor
-    if next(model.parameters()).is_cuda:
-        tensor = tensor.cuda()
+def profile(model, input_shape=(1, 3, 2048, 1024), profiling=True, speedtest=1000):
     if speedtest:
+        warmed = False
+        tensor = torch.rand(input_shape)  # creating input tensor
+        if next(model.parameters()).is_cuda:
+            tensor = tensor.cuda()
+            time_counter = torch.zeros((speedtest + 2, 1))
+            time0, time1 = torch.cuda.Event(enable_timing=True), torch.cuda.Event(
+                enable_timing=True
+            )
+            logger.log("running GPU side synchronized speed test...")
+            model.eval()
+            with torch.no_grad():
+                logger.log("start warm up")
+                for i in tqdm(range(10)):
+                    model(tensor)
+                warmed = True
+                logger.log("warm up done")
+                for test_index in tqdm(range(speedtest + 2)):
+                    time0.record()
+                    model(tensor)
+                    time1.record()
+                    torch.cuda.synchronize()
+                    time_counter[test_index] = time0.elapsed_time(time1)
+            _min, _max, _sum = (
+                time_counter.min(),
+                time_counter.max(),
+                time_counter.sum(),
+            )
+            _aver = (_sum - _min - _max) / speedtest
+            logger.log(f"average {_aver / 1000.}s per run with input size {input_shape}")
+            logger.log(f"min inference time: {_min / 1000.}s, max inference time: {_max / 1000.}s")
+            logger.log(f"That is {(1. / _aver) * 1000.} frames per second")
+            logger.log("====================================================")
+
         counter = []
-        logger.log("running LatencyTest...")
+        logger.log("running Latency test...")
         model.eval()
         with torch.no_grad():
-            logger.log("start warm up")
-            for i in tqdm(range(10)):
-                model(tensor)
-            logger.log("warm up done")
-            logger.log("calculating FPS...")
-            for test_index in tqdm(range(speedtest+2)):
+            if not warmed:
+                logger.log("start warm up")
+                for i in tqdm(range(10)):
+                    model(tensor)
+                logger.log("warm up done")
+            for test_index in tqdm(range(speedtest + 2)):
                 t = time.time()
                 model(tensor)
                 counter.append(time.time() - t)
             _min, _max, _sum = min(counter), max(counter), sum(counter)
-            aver = (_sum - _min - _max) / (len(counter) - 2)
-            logger.log(f"speedTest: average {aver}s per run with input size {input_shape}")
-            logger.log(f"min inference time: {_min}s, Max inference time: {_max}")
-            logger.log(f"FPS: {speedtest / _sum} per second")
-        
+        _aver = (_sum - _min - _max) / speedtest
+        logger.log(f"average {_aver}s per run with input size {input_shape}")
+        logger.log(f"min inference time: {_min}s, Max inference time: {_max}s")
+        logger.log(f"That is {1. / _aver} frames per second")
+
     if profiling:
+        logger.log("====================================================")
         logger.log("model profiling...")
         tensor = torch.rand(input_shape)
         if next(model.parameters()).is_cuda:
