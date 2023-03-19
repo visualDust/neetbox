d6108d9a9ff26762c9f13c9f9d963d70fe223401
VisualDust <gavin@gong.host>
2023-03-19 15:28:53 +0800

    repacked tests

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,33 +1,35 @@
-from neetbox.logging import logger
+def test_default_logger():
+    from neetbox.logging import logger
 
-logger.info("loginfo")
-logger.warn("logwarn")
-logger.err("logerr")
+    logger.info("loginfo")
+    logger.warn("logwarn")
+    logger.err("logerr")
 
-def a():
-    logger.log("Hello from the default logger")
-    
-class A():
-    def a(self):
+    def a():
         logger.log("Hello from the default logger")
 
-A().a()
+    class A():
+        def a(self):
+            logger.log("Hello from the default logger")
 
-from neetbox.logging.logger import get_logger
-logger = get_logger("someone")
-logger.set_log_dir("./logdir")
-logger.log("someone said 1")
-def b():
-    logger.log("a")
-def c():
-    b()
-    logger.log("b")
-c()
-logger.log("someone said 2")
+    A().a()
 
-class B:
-    def b(self):
-        self.logger = get_logger(self)
-        self.logger.log("from class B")
-        
-B().b()
+def test_logger_with_specific_identity():
+    from neetbox.logging.logger import get_logger
+    logger = get_logger("someone")
+    logger.set_log_dir("./logdir")
+    logger.log("someone said 1")
+    def b():
+        logger.log("a")
+    def c():
+        b()
+        logger.log("b")
+    c()
+    logger.log("someone said 2")    
+
+    class B:
+        def b(self):
+            self.logger = get_logger(self)
+            self.logger.log("from class B")
+
+    B().b()
