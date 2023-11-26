12d64ba2888aee1db9bce76bef419a70b2573c3f
lideming <me@yuuza.net>
2023-11-26 18:01:28 +0800

    feat: dynamic import echarts

---
+++ neetbox/frontend/src/components/loading.tsx
@@ -0,0 +1,23 @@
+import { Spin } from "@douyinfe/semi-ui";
+
+export default function Loading({
+  width = "",
+  height = "100px",
+}: {
+  width?: string;
+  height?: string;
+}) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width,
+        height,
+      }}
+    >
+      <Spin size="large" />
+    </div>
+  );
+}
