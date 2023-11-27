5e2089509dce2be8e4e1414628b84b9b376009d3
lideming <me@yuuza.net>
2023-11-27 21:37:09 +0800

    fix: ws in dev mode

--- neetbox/frontend/src/services/api.ts
+++ neetbox/frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import useSWR from "swr";
 
 export const API_BASEURL = "/web";
 export const WEBSOCKET_URL = import.meta.env.DEV
-  ? "ws://localhost:5001"
+  ? `ws://${location.host}/ws/`
   : `ws://${location.hostname}:${Number(location.port) + 1}`;
 
 async function fetcher(url: string) {
--- neetbox/frontend/vite.config.ts
+++ neetbox/frontend/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import SemiPlugin from "vite-plugin-semi-theme";
 
+const server = new URL('http://127.0.0.1:5000');
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), SemiPlugin({
@@ -10,14 +12,15 @@ export default defineConfig({
   server: {
     proxy: {
       '/web/': {
-        target: 'http://127.0.0.1:5000',
+        target: server.href,
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api\//, ''),
       },
       '/ws/': {
-        target: 'http://127.0.0.1:5001',
+        target: `ws://${server.host}:${(+server.port) + 1}`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/ws\//, ''),
+        ws: true,
       }
     }
   },
