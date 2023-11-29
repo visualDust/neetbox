5a942c4b8cd9465d4812eb997e96c5f94e51a37d
lideming <me@yuuza.net>
2023-11-29 10:48:24 +0800

    perf: prevent useless renders

--- neetbox/frontend/src/components/dashboard/project/actions.tsx
+++ neetbox/frontend/src/components/dashboard/project/actions.tsx
@@ -8,9 +8,10 @@ import {
   Space,
   Typography,
 } from "@douyinfe/semi-ui";
-import { useContext, useState } from "react";
+import { memo, useContext, useState } from "react";
 import { ProjectStatus, getProject } from "../../../services/projects";
 import { ProjectContext } from "../../../pages/console/proejctDashboard";
+import { useMemoJSON } from "../../../hooks/useMemoJSON";
 
 interface Props {
   actions: ProjectStatus["__action"];
@@ -18,7 +19,7 @@ interface Props {
 
 export function Actions({ actions }: Props) {
   const [blocking, setBlocking] = useState(false);
-  const actionList = Object.entries(actions?.value ?? {});
+  const actionList = Object.entries(useMemoJSON(actions?.value ?? {}));
   return (
     <Space style={{ marginBottom: "20px" }} spacing="medium" wrap>
       {actionList.length ? (
@@ -51,108 +52,110 @@ interface ActionItemProps {
   setBlocking: (blocking: boolean) => void;
 }
 
-export function ActionItem({
-  name,
-  actionOptions: options,
-  blocking,
-  setBlocking,
-}: ActionItemProps) {
-  const [args, setArgs] = useState<Record<string, string>>(() =>
-    Object.fromEntries(
-      Object.entries(options.args).map(([name, type]) => [
-        name,
-        type == "str" ? '""' : type == "bool" ? "False" : "",
-      ])
-    )
-  );
-  const [running, setCurrentBlocking] = useState(false);
-  const [result, setResult] = useState<string | null>(null);
-  const { projectName } = useContext(ProjectContext)!;
-  const handleRun = () => {
-    if (options.blocking) setBlocking(true);
-    setCurrentBlocking(true);
-    getProject(projectName).sendAction(
-      name,
-      args,
-      ({ error: err, result: res }) => {
-        if (options.blocking) setBlocking(false);
-        setCurrentBlocking(false);
-        setResult(err ? `error:\n${err}` : `result:\n${JSON.stringify(res)}`);
-      }
+export const ActionItem = memo(
+  ({
+    name,
+    actionOptions: options,
+    blocking,
+    setBlocking,
+  }: ActionItemProps) => {
+    const [args, setArgs] = useState<Record<string, string>>(() =>
+      Object.fromEntries(
+        Object.entries(options.args).map(([name, type]) => [
+          name,
+          type == "str" ? '""' : type == "bool" ? "False" : "",
+        ]),
+      ),
     );
-  };
-  const renderContent = () => (
-    <Space
-      vertical
-      spacing={"tight"}
-      style={{ padding: "10px", minWidth: "200px", maxWidth: "500px" }}
-    >
-      <Typography.Title heading={5}>{name}</Typography.Title>
-      {options.description && (
-        <div style={{ margin: 0, whiteSpace: "pre-wrap" }}>
-          {options.description}
-        </div>
-      )}
-      {Object.entries(options.args).map(([argName, argType]) => (
-        <Row
-          key={argName}
-          align="middle"
-          type="flex"
-          justify="space-between"
-          style={{ alignSelf: "stretch" }}
-        >
-          <Col span={6}>
-            <Typography.Text ellipsis={{ showTooltip: true }}>
-              {argName}
-            </Typography.Text>
-          </Col>
-          <Col span={12}>
-            {argType == "bool" ? (
-              <Checkbox
-                checked={args[argName] == "True"}
-                onChange={(val) =>
-                  setArgs({
-                    ...args,
-                    [argName]: args[argName] == "True" ? "False" : "True",
-                  })
-                }
-              >
-                {args[argName]}
-              </Checkbox>
-            ) : (
-              <Input
-                size="small"
-                value={args[argName]}
-                onChange={(val) => setArgs({ ...args, [argName]: val })}
-              />
-            )}
-          </Col>
-          <Col span={6}>
-            <Typography.Text ellipsis={{ showTooltip: true }}>
-              ({argType})
-            </Typography.Text>
-          </Col>
-        </Row>
-      ))}
-      <Button
-        style={{ width: "100px" }}
-        onClick={handleRun}
-        type="warning"
-        theme="solid"
-        disabled={blocking}
+    const [running, setCurrentBlocking] = useState(false);
+    const [result, setResult] = useState<string | null>(null);
+    const { projectName } = useContext(ProjectContext)!;
+    const handleRun = () => {
+      if (options.blocking) setBlocking(true);
+      setCurrentBlocking(true);
+      getProject(projectName).sendAction(
+        name,
+        args,
+        ({ error: err, result: res }) => {
+          if (options.blocking) setBlocking(false);
+          setCurrentBlocking(false);
+          setResult(err ? `error:\n${err}` : `result:\n${JSON.stringify(res)}`);
+        },
+      );
+    };
+    const renderContent = () => (
+      <Space
+        vertical
+        spacing={"tight"}
+        style={{ padding: "10px", minWidth: "200px", maxWidth: "500px" }}
       >
-        Run
-      </Button>
-      {result && (
-        <div style={{ margin: 0, whiteSpace: "pre-wrap" }}>{result}</div>
-      )}
-    </Space>
-  );
-  return (
-    <Popover trigger="click" content={renderContent()}>
-      <Button disabled={blocking && !running} loading={running}>
-        {name}
-      </Button>
-    </Popover>
-  );
-}
+        <Typography.Title heading={5}>{name}</Typography.Title>
+        {options.description && (
+          <div style={{ margin: 0, whiteSpace: "pre-wrap" }}>
+            {options.description}
+          </div>
+        )}
+        {Object.entries(options.args).map(([argName, argType]) => (
+          <Row
+            key={argName}
+            align="middle"
+            type="flex"
+            justify="space-between"
+            style={{ alignSelf: "stretch" }}
+          >
+            <Col span={6}>
+              <Typography.Text ellipsis={{ showTooltip: true }}>
+                {argName}
+              </Typography.Text>
+            </Col>
+            <Col span={12}>
+              {argType == "bool" ? (
+                <Checkbox
+                  checked={args[argName] == "True"}
+                  onChange={(val) =>
+                    setArgs({
+                      ...args,
+                      [argName]: args[argName] == "True" ? "False" : "True",
+                    })
+                  }
+                >
+                  {args[argName]}
+                </Checkbox>
+              ) : (
+                <Input
+                  size="small"
+                  value={args[argName]}
+                  onChange={(val) => setArgs({ ...args, [argName]: val })}
+                />
+              )}
+            </Col>
+            <Col span={6}>
+              <Typography.Text ellipsis={{ showTooltip: true }}>
+                ({argType})
+              </Typography.Text>
+            </Col>
+          </Row>
+        ))}
+        <Button
+          style={{ width: "100px" }}
+          onClick={handleRun}
+          type="warning"
+          theme="solid"
+          disabled={blocking}
+        >
+          Run
+        </Button>
+        {result && (
+          <div style={{ margin: 0, whiteSpace: "pre-wrap" }}>{result}</div>
+        )}
+      </Space>
+    );
+    return (
+      <Popover trigger="click" content={renderContent}>
+        <Button disabled={blocking && !running} loading={running}>
+          {name}
+        </Button>
+      </Popover>
+    );
+  },
+);
--- neetbox/frontend/src/components/dashboard/project/platformProps.tsx
+++ neetbox/frontend/src/components/dashboard/project/platformProps.tsx
@@ -1,66 +1,81 @@
-import React from "react";
+import React, { memo } from "react";
 import { Toast, Button, Card, CardGroup, Typography } from "@douyinfe/semi-ui";
 import { IconCopy } from "@douyinfe/semi-icons";
+import { useMemoJSON } from "../../../hooks/useMemoJSON";
+import { ProjectStatus } from "../../../services/projects";
 
-function PropCard({ propName, propValue }): React.JSX.Element {
-  const { Text } = Typography;
-  const content = Array.isArray(propValue) ? propValue.join(" ") : propValue;
-  const nameMapping = {
-    username: "Launched by",
-    machine: "Machine type",
-    processor: "Processor name",
-    os_name: "System/OS name",
-    os_release: "Sys release ver",
-    architecture: "Python arch.",
-    python_build: "Python build",
-    python_version: "Python version",
-  };
-  return (
-    <Card
-      shadows="hover"
-      title={nameMapping[propName] ?? propName}
-      headerLine={true}
-      headerStyle={{ padding: "10px" }}
-      bodyStyle={{
-        padding: "10px",
-        minHeight: "50px",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        textAlign: "center",
-      }}
-      style={{ width: 260 }}
-      headerExtraContent={
-        <Button
-          icon={<IconCopy />}
-          style={{ marginRight: 10 }}
-          size="small"
-          onClick={() => {
-            navigator.clipboard.writeText(propValue).then(
-              () => {
-                // copy success
-                Toast.info("Copied to clipboard");
-              },
-              () => {
-                // copy failed
-                Toast.error("Failed to copy");
-              },
-            );
-          }}
-        ></Button>
-      }
-    >
-      <Text>{content}</Text>
-    </Card>
-  );
-}
+const PropCard = memo(
+  ({
+    propName,
+    propValue,
+  }: {
+    propName: string;
+    propValue: ProjectStatus["platform"]["value"][string];
+  }) => {
+    const { Text } = Typography;
+    const content = Array.isArray(propValue) ? propValue.join(" ") : propValue;
+    const nameMapping = {
+      username: "Launched by",
+      machine: "Machine type",
+      processor: "Processor name",
+      os_name: "System/OS name",
+      os_release: "Sys release ver",
+      architecture: "Python arch.",
+      python_build: "Python build",
+      python_version: "Python version",
+    };
+    return (
+      <Card
+        shadows="hover"
+        title={nameMapping[propName] ?? propName}
+        headerLine={true}
+        headerStyle={{ padding: "10px" }}
+        bodyStyle={{
+          padding: "10px",
+          minHeight: "50px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          textAlign: "center",
+        }}
+        style={{ width: 260 }}
+        headerExtraContent={
+          <Button
+            icon={<IconCopy />}
+            style={{ marginRight: 10 }}
+            size="small"
+            onClick={() => {
+              navigator.clipboard.writeText(content).then(
+                () => {
+                  // copy success
+                  Toast.info("Copied to clipboard");
+                },
+                () => {
+                  // copy failed
+                  Toast.error("Failed to copy");
+                },
+              );
+            }}
+          ></Button>
+        }
+      >
+        <Text>{content}</Text>
+      </Card>
+    );
+  },
+);
 
-export default function PlatformProps({ data }): React.JSX.Element {
+export default function PlatformProps({
+  data,
+}: {
+  data: ProjectStatus["platform"];
+}): React.JSX.Element {
+  const memoData = useMemoJSON(data?.value);
   return (
     <div>
       <CardGroup spacing={10}>
-        {(data?.value &&
-          Object.entries(data.value).map(([key, value]) => (
+        {(memoData &&
+          Object.entries(memoData).map(([key, value]) => (
             <PropCard key={key} propName={key} propValue={value} />
           ))) || <Typography.Text>No Platform Info</Typography.Text>}
       </CardGroup>
