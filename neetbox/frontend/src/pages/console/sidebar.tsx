a33a0b0ae90228061c26514f00b4d0abafa15e26
lideming <me@yuuza.net>
2023-12-01 18:30:06 +0800

    fix: remove sidebar link wrapper

--- neetbox/frontend/src/pages/console/sidebar.tsx
+++ neetbox/frontend/src/pages/console/sidebar.tsx
@@ -1,23 +1,18 @@
 import { Nav } from "@douyinfe/semi-ui";
-import React from "react";
 import { IconHome, IconListView } from "@douyinfe/semi-icons";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useAPI } from "../../services/api";
 import Loading from "../../components/loading";
 
 export default function ConsoleNavBar() {
   const location = useLocation();
+  const navigate = useNavigate();
   const { data } = useAPI("/list", { refreshInterval: 5000 });
   return (
     <Nav
       renderWrapper={(args) => {
         if (args.props.itemKey === "loading") return <Loading height="60px" />;
-        if (!(args.props.itemKey as string).startsWith("/")) return args.itemElement;
-        return (
-          <Link to={args.props.itemKey as string} style={{ textDecoration: "none" }}>
-            {args.itemElement}
-          </Link>
-        );
+        return args.itemElement;
       }}
       style={{ height: "100%", overflowY: "auto" }}
       items={[
@@ -34,6 +29,11 @@ export default function ConsoleNavBar() {
             : [{ text: "", itemKey: "loading" }],
         },
       ]}
+      onClick={(data) => {
+        if ((data.itemKey as string).startsWith("/")) {
+          navigate(data.itemKey as string);
+        }
+      }}
       defaultOpenKeys={["projects"]}
       selectedKeys={[location.pathname]}
       footer={{
