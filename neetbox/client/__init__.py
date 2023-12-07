b875bcfe89212b95c08a54524d2f4e892dfbab8b
VisualDust <gavin@gong.host>
2023-12-07 23:34:34 +0800

    rename: add_scatter to add_scalar

--- neetbox/client/__init__.py
+++ neetbox/client/__init__.py
@@ -6,10 +6,10 @@
 
 from ._client_action_agent import _NeetActionManager as NeetActionManager
 from ._image import add_image
-from ._plots import add_scatter
+from ._plots import add_scalar
 from ._signal_and_slot import listen, watch
 
 action = NeetActionManager.register
 
 add_tensor = add_image
-__all__ = ["add_image", "add_tensor", "add_scatter", "action", "NeetActionManager", "watch", "listen"]
+__all__ = ["add_image", "add_tensor", "add_scalar", "action", "NeetActionManager", "watch", "listen"]
--- neetbox/client/_plots.py
+++ neetbox/client/_plots.py
@@ -1,17 +1,15 @@
-import io
 from typing import Union
-
 from neetbox._daemon._protocol import *
 from neetbox._daemon.client._client import connection
-from neetbox.config import get_project_id, get_run_id
 
 
-def add_scatter(name: str, x: Union[int, float], y: Union[int, float]):
+def add_scalar(name: str, x: Union[int, float], y: Union[int, float]):
     """send an image to frontend display
 
     Args:
         image (Union[np.array, Image.Image]): image from cv2 and PIL.Image are supported
         name (str): name of the image, used in frontend display
     """
-    # send bytes
-    connection.ws_send(event_type="scatter", payload={"series": name, "x": x, "y": y})
+    # send
+    connection.ws_send(event_type="scalar", payload={SERIES_ID_KEY: name, "x": x, "y": y})
+    
