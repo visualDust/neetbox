68858d63fead3974b9924575a1fb424a7095efdc
lideming <me@yuuza.net>
2023-12-08 14:04:00 +0800

    fix: lint

--- frontend/src/components/appTitle.tsx
+++ frontend/src/components/appTitle.tsx
@@ -9,7 +9,6 @@ export const AppTitle = (props: PropsWithChildren<{ extra?: ReactNode }>) => {
   const [_, setTitle] = useAtom(appTitle);
   useEffect(() => {
     setTitle((arr) => [...arr, { title: props.children, extra: props.extra }]);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     return () => setTitle((arr) => arr.slice(0, -1));
   }, [props.children, props.extra, setTitle]);
   return null;
