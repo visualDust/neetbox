5986daee5c0bd478b55f8f7de3c0665a2f5fb6bb
VisualDust <gavin@gong.host>
2023-12-20 22:00:23 +0800

    fix "repeated axis in transpose" error when add tensor as image

--- neetbox/client/_image.py
+++ neetbox/client/_image.py
@@ -95,24 +95,29 @@ def convert_to_HWC(tensor, input_format):  # tensor: numpy array
     )
     input_format = input_format.upper()
 
-    if len(input_format) == 4:
-        index = [input_format.find(c) for c in "NCHW"]
-        tensor_NCHW = tensor.transpose(index)
-        tensor_CHW = make_grid(tensor_NCHW)
-        return tensor_CHW.transpose(1, 2, 0)
-
-    if len(input_format) == 3:
-        index = [input_format.find(c) for c in "HWC"]
-        tensor_HWC = tensor.transpose(index)
-        if tensor_HWC.shape[2] == 1:
-            tensor_HWC = np.concatenate([tensor_HWC, tensor_HWC, tensor_HWC], 2)
-        return tensor_HWC
-
     if len(input_format) == 2:
         index = [input_format.find(c) for c in "HW"]
         tensor = tensor.transpose(index)
         tensor = np.stack([tensor, tensor, tensor], 2)
         return tensor
+
+    if len(input_format) == 3:
+        if "N" not in input_format:
+            index = [input_format.find(c) for c in "HWC"]
+            tensor_HWC = tensor.transpose(index)
+            if tensor_HWC.shape[2] == 1:
+                tensor_HWC = np.concatenate([tensor_HWC, tensor_HWC, tensor_HWC], 2)
+            return tensor_HWC
+        else:
+            index = [input_format.find(c) for c in "NHW"]
+            tensor = tensor.transpose(index)[:, None, :, :]
+            input_format = "NCHW"
+
+    if len(input_format) == 4:
+        index = [input_format.find(c) for c in "NCHW"]
+        tensor_NCHW = tensor.transpose(index)
+        tensor_CHW = make_grid(tensor_NCHW)
+        return tensor_CHW.transpose(1, 2, 0)
 
 
 def add_image(name: str, image, dataformats: str = None):
