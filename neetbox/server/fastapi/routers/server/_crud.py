8af9d018553e54f78fbd80baacb98d32a5532156
VisualDust <gavin@gong.host>
2024-01-20 17:51:55 +0800

    add api/server/listips

--- neetbox/server/fastapi/routers/server/_crud.py
+++ neetbox/server/fastapi/routers/server/_crud.py
@@ -5,6 +5,7 @@
 # Date:   20240120
 
 import os
+import socket
 import time
 from threading import Thread
 
@@ -22,6 +23,14 @@ router = APIRouter()
 @router.get("/hello")
 async def just_send_hello():
     return {"hello": "hello"}
+
+
+@router.get("/listips")
+async def list_server_ips():
+    local_hostname = socket.gethostname()
+    ip_addresses = socket.gethostbyname_ex(local_hostname)[2]
+    filtered_ips = [ip for ip in ip_addresses if not ip.startswith("127.")]
+    return {"hostname": local_hostname, "ips": filtered_ips}
 
 
 @router.post(f"/shutdown")
